7f6b97d502b6b1c9c3422651108d0b184a23abce
chansereipanha <sereipanhachanpersonal@gmail.com>
2026-08-19 12:49:30 +1200

    feat: Add initial default client and library routes chore: changed client routing to a dynamic one with [clientId] instead

---
+++ app/clients/[clientId]/page.tsx
@@ -0,0 +1,3 @@
+export default function ClientProfilePage() {
+    return <main><h1>Client profile</h1></main>;
+}
---
+++ app/clients/page.tsx
@@ -0,0 +1,3 @@
+export default function ClientsPage() {
+    return <main><h1>Clients</h1></main>;
+}
